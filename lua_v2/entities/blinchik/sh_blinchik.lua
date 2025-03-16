b4b9bd0939bc2190383ece55389ae103b0983f4d
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-03-16 09:10:57 -0700

    Create sh_blinchik.lua

---
+++ lua_v2/entities/blinchik/sh_blinchik.lua
@@ -0,0 +1,23 @@
+ENT.Base = "base_gmodentity";
+
+ENT.Type = "anim";
+
+ENT.Category = "3DEMC_models"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "Blinchik"
+
+ENT.PrintName = "Blinchik"
+
+ENT.IconOverride = "entities/blin1.png"
+
+ENT.Author = "FURA"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = "Блинчик с аппарата."
+
+ENT.Instructions = "Навестись на блинчик. Посмотреть информация. Скушать."

e6e38eaf4ddd2882deddaaaef594291bd42fe0d8
FURUSHKA <163753122+FYURi4@users.noreply.github.com>
2025-03-16 09:09:48 -0700

    Create sh_blindozer.lua

---
+++ lua_v2/entities/blindozer/sh_blindozer.lua
@@ -0,0 +1,23 @@
+ENT.Base = "base_gmodentity";
+
+ENT.Type = "anim";
+
+ENT.Category = "3DEMC_models"
+
+ENT.Spawnable = true
+
+ENT.AdminSpawnable = true
+
+ENT.ClassName = "Blindozer"
+
+ENT.PrintName = "Blindozer"
+
+ENT.Author = "FURA"
+
+ENT.IconOverride = "entities/blindozer.png"
+
+ENT.Contact = "Discord: fyurl4, Furushka: FurichF"
+
+ENT.Purpose = "Создание блинчиков."
+
+ENT.Instructions = "Подойти к модели, нажать 'E'. Пользоваться :З"
